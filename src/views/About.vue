b6ac118c76fd7f679ddf1051fb3aa2cec759abb2
axiever-tousif <tousif@axiever.com>
2025-07-14 18:24:51 +0530

    update

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Hi, I'm Tousif Anjum</h1>
+    <p>I'm a Frontend Developer passionate about Vue.js.</p>
+    <router-link to="/projects">View My Projects</router-link>
+  </div>
+</template>
